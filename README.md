9ffb50772a1823f9962424005f5dfcc00ce34403
Maciel <a60848@alunos.ipb.pt>
2026-01-20 20:32:28 +0000

    Add main code

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 Hello World
-=======
-Hell oWorld
->>>>>>> 9409f3d48c4318f729fa0c04d39526aebff5c637
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+
+    int myNum1 = 50;
+    int myNum2 = 100;
+
+    System.out.println(myNum1 + myNum2);
+  }
+}
+
