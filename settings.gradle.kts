1c82cbae3605a6112b7fbef7b383b10d9644aff7
Maciel <a60848@alunos.ipb.pt>
2026-01-22 00:51:33 +0000

    Some files were missing. Now the build need to fail cause of an error

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,10 @@
  * This project uses @Incubating APIs which are subject to change.
  */
 
-rootProject.name = "teste"
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+rootProject.name = "cicd-training-lab"
+include("app")
