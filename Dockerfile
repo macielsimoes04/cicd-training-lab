b8336f8ff6c2e2730acf52ee134e73d85bbcac8d
Maciel <a60848@alunos.ipb.pt>
2026-01-23 05:46:35 +0000

    test

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,3 @@ WORKDIR /app
 COPY --from=builder ./app/build/libs/*.jar app.jar
 
 ENTRYPOINT [ "java", "-jar", "app.jar" ]
-
-
--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println(1 + 4);
+    System.out.println(1 + 3);
   }
 }
